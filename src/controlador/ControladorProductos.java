170efb587118a0677edc994bcf403481609c38df
Unknown <c.ettorre@gmail.com>
2018-03-08 16:19:35 +0100

    Producto bean and database connection using context.xml
    created context.xml to manage pool connectio to database
    
    creted neans Productos
    
    creted ModeloProductos to connect to MySql DB

---
+++ src/controlador/ControladorProductos.java
@@ -0,0 +1,46 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.ModeloProductos;
+
+/**
+ * Servlet implementation class ControladorProductos
+ */
+@WebServlet("/ControladorProductos")
+public class ControladorProductos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private ModeloProductos modeloProductos;
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControladorProductos() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/modelo/ModeloProductos.java
@@ -0,0 +1,54 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
+import beans.*;
+
+public class ModeloProductos {
+	
+	private DataSource origenDatos;
+	
+	public ModeloProductos(DataSource origenDatos) {
+		
+		this.origenDatos=origenDatos;
+		
+	}
+
+	
+	public List<Productos> getProductos() throws Exception{
+		
+		List<Productos> productos=new ArrayList<>();
+		
+		Connection miConexion=null;
+		Statement miStatement =null;
+		ResultSet miResultset=null;
+		
+		miConexion=origenDatos.getConnection();
+		String instruccionSql="SELECT * FROM PRODUCTOS";
+		miResultset=miStatement.executeQuery(instruccionSql);//tabla virtual obtenida
+		
+		while(miResultset.next()) {
+			String cArt=miResultset.getString("CÓDIGOARTÍCULO");
+			String seccion=miResultset.getString("SECCIÓN");
+			String nArt=miResultset.getString("NOMBREARTÍCULO");
+			double precio=miResultset.getDouble("PRECIO");
+			Date fecha=miResultset.getDate("FECHA");
+			String importado=miResultset.getString("IMPORTADO");
+			String pOrigen=miResultset.getString("PAÍSORIGEN");
+			
+			Productos temProd = new Productos(cArt, seccion, nArt, precio, fecha, importado, pOrigen);
+			
+			productos.add(temProd);
+		}
+		
+		return productos; 
+		
+	}
+	
+}
